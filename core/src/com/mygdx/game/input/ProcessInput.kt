e56fe13ea7bb98feb26897037ebe35cae393cc42
Maximilian Wittmann <damxam@gmail.com>
2020-03-15 17:14:55 +1100

    Cleanup

--- core/src/com/mygdx/game/input/ProcessInput.kt
+++ core/src/com/mygdx/game/input/ProcessInput.kt
@@ -3,16 +3,12 @@ package com.mygdx.game.input
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.mygdx.game.entities.World
-import com.mygdx.game.util.Point2
 
 import arrow.core.compose
 import com.mygdx.game.entities.Terrain
 import com.mygdx.game.util.Cardinality
-import com.mygdx.game.util.Rect2
-import com.mygdx.game.util.Vec2
 
 const val moveAmount = 5
-//const val moveAmount = 1
 
 val readKey: (Unit) -> (Set<Key>) = {
     val keys = emptySet<Key>().toMutableSet()
@@ -40,62 +36,19 @@ val processKeys: (Set<Key>) -> (Set<Action>) = { keys: Set<Key> ->
     actions
 }
 
-//fun isMoveCanPassTerrain(): (World) -> (Point2) -> Boolean = { world ->
-//    { newPosition: Point2 ->
-//        world.getTerrainAt(newPosition).map { terrain -> terrain.prototype.attributes.passable }.getOrElse { false }
-//    }
-//}
-//
-//fun doMove
 
 val processActions: (World) -> (Set<Action>) -> Unit = { world ->
     { actions ->
         for (action in actions) {
-            when (action) {
-                Action.MOVE_UP -> {
-                    val moveAmount = movePlayer(world.worldObjects.player.rect(), 50, world.terrain, { t: Terrain -> t.prototype.attributes.passable }, moveAmount, Cardinality.UP)
-                    world.movePlayer(moveAmount)
-
-//                    world.movePlayer(Vec2(0, moveAmount))
-//                    world.movePlayer(moveAmount, Cardinality.UP)
-//                    val newPos: Point2 = world.worldObjects.player.position.plus(moveVec)
-//                    if (isMoveCanPassTerrain()(world)(newPos)) {
-//                        world.movePlayer(moveVec)
-////                        world.worldObjects.player.position = world.worldObjects.player.position.plus(newPos)
-////                        world.view = world.view.plus(0, moveAmount)
-//                    }
-                }
-                Action.MOVE_DOWN -> {
-                    val moveAmount = movePlayer(world.worldObjects.player.rect(), 50, world.terrain, { t: Terrain -> t.prototype.attributes.passable }, moveAmount, Cardinality.DOWN)
-                    world.movePlayer(moveAmount)
-
-//                    world.movePlayer(Vec2(0, -moveAmount))
-//                    world.movePlayer(moveAmount, Cardinality.DOWN)
-//                    val newPos: Point2 = world.worldObjects.player.position.minus(Point2(0, moveAmount))
-//                    world.worldObjects.player.position = world.worldObjects.player.position.minus(newPos)
-//                    world.view = world.view.minus(0, moveAmount)
-                }
-                Action.MOVE_LEFT -> {
-                    val moveAmount = movePlayer(world.worldObjects.player.rect(), 50, world.terrain, { t: Terrain -> t.prototype.attributes.passable }, moveAmount, Cardinality.LEFT)
-                    world.movePlayer(moveAmount)
-
-//                    world.movePlayer(Vec2(-moveAmount, 0))
-//                    world.movePlayer(moveAmount, Cardinality.LEFT)
-//                    val newPos: Point2 = world.worldObjects.player.position.minus(Point2( moveAmount, 0))
-//                    world.worldObjects.player.position = world.worldObjects.player.position.minus(newPos)
-//                    world.updateView(-moveAmount, 0)
-                }
-                Action.MOVE_RIGHT -> {
-                    val moveAmount = movePlayer(world.worldObjects.player.rect(), 50, world.terrain, { t: Terrain -> t.prototype.attributes.passable }, moveAmount, Cardinality.RIGHT)
-                    world.movePlayer(moveAmount)
-
-//                    world.movePlayer(Vec2(moveAmount, 0))
-//                    world.movePlayer(moveAmount, Cardinality.RIGHT)
-//                    val newPos: Point2 = world.worldObjects.player.position.plus(Point2(moveAmount, 0))
-//                    world.worldObjects.player.position = world.worldObjects.player.position.plus(newPos)
-//                    world.updateView(moveAmount, 0)
-                }
+            val cardinality = when (action) {
+                Action.MOVE_UP -> Cardinality.UP
+                Action.MOVE_DOWN -> Cardinality.DOWN
+                Action.MOVE_LEFT -> Cardinality.LEFT
+                Action.MOVE_RIGHT -> Cardinality.RIGHT
             }
+            val moveBy = movePlayer(world.worldObjects.player.rect(), 50, world.terrain,
+                    { t: Terrain -> t.prototype.attributes.passable }, moveAmount, cardinality)
+            world.movePlayer(moveBy)
         }
     }
 }
